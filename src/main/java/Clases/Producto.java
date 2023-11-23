f77cb6253904961d6f830ee4a9d209b3e04a8b75
Benttyman <jesustapiamartin@gmail.com>
2023-11-23 10:28:34 -0300

    Clase Producto en el Package Clases Clase CsvProductos con todas las funcionalidades relacionadas crear el csv de productos, agregar, listar y eliminar

--- src/main/java/Clases/Producto.java
+++ src/main/java/Clases/Producto.java
@@ -1,4 +1,42 @@
 package Clases;
 
 public class Producto {
+
+    // ===== ATRIBUTOS =====
+    private int     id      , cantidad;
+    private String  nombre;
+
+
+    // ===== CONSTRUCTOR =====
+    public Producto(int id, String nombre, int cantidad) {
+        this.id         = id;
+        this.nombre     = nombre;
+        this.cantidad   = cantidad;
+    }
+
+
+    // ===== GETTER Y SETTER =====
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
